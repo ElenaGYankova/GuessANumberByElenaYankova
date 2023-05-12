70f6641f883d8e384371872b4bedff39ef706a87
Elena Yankova <122468758+ElenaGYankova@users.noreply.github.com>
2023-05-12 18:32:43 +0300

    Add files via upload

---
+++ guess_a_number.py
@@ -0,0 +1,23 @@
+import random
+from colorama import Fore, Back
+
+print("Welcome to Guess A Number game!")
+print("I'm thinking of a number between 1 and 100.")
+print("You have to guess the number in 7 attempts.")
+
+number = random.randint(1, 100)
+guess_count = 0
+
+while guess_count < 7:
+    guess = int(input(Fore.LIGHTBLUE_EX + "Enter your guess: "))
+    guess_count += 1
+    if guess < number:
+        print(Fore.LIGHTGREEN_EX + "Your guess is too low.")
+    elif guess > number:
+        print(Fore.RED + "Your guess is too high.")
+    else:
+        print(Fore.GREEN + "Congratulations! You guessed the number in", guess_count, "attempts.")
+        break
+
+else:  # if guess_count == 8:
+    print(Back.LIGHTMAGENTA_EX + "Sorry, you did not guess the number. The number was", number)
